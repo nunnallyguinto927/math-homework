dd584a18dc084e83e2699d9f4c9304bb53c0ee0a
nunnallyguinto927 <nunnallyguinto927@gmail.com>
2025-03-31 06:14:30 +0000

    Add 21.rs

---
+++ src/21.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Example Rust code to perform basic arithmetic operations
+    let num1 = 5;
+    let num2 = 3;
+    println!("The result of {} + {} is {}", num1, num2, num1 + num2);
+}
