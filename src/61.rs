2b22a9e25de6688815dc55432a0c28cf79d83ef8
nunnallyguinto927 <nunnallyguinto927@gmail.com>
2025-04-25 20:36:56 +0000

    Add 61.rs

---
+++ src/61.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let num1 = 5;
+    let num2: i32 = 10;
+    
+    if num1 > num2 {
+        println!("num1 is greater than num2");
+    } else {
+        println!("num2 is greater than num1");
+    }
+}
